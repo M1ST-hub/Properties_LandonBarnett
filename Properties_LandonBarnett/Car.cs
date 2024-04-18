6228045200161ca3a0ccdb380151b056c28456bd
M1ST-hub <nmckrypt@gmail.com>
2024-04-18 10:09:13 -0500

    Initial commit

---
+++ Properties_LandonBarnett/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_LandonBarnett
+{
+    internal class Car
+    {
+
+        private string model;
+
+        public string Model { get { return model; } set { model = value; } }
+
+        public string Make { get; set; }
+    }
+}
---
+++ Properties_LandonBarnett/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Properties_LandonBarnett
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Car car = new Car();
+            car.Make = "Ford";
+            car.Model = "Mustang";
+
+            Console.WriteLine($"Make: {car.Make}, Model: {car.Model}");
+
+            Car car2 = new Car();
+            car2.Make = "Chevy";
+            car2.Model = "Camero";
+
+            Console.WriteLine($"Make: {car2.Make}, {car2.Model}");
+
+        }
+    }
+}
